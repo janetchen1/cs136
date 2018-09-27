6923aa38f4be47991898f9f7f214584213eec764
laurenkim1 <laurenkim@college.harvard.edu>
2018-09-27 10:54:44 -0400

    fixed request for std

--- lkjcstd.py
+++ lkjcstd.py
@@ -13,7 +13,7 @@ from messages import Upload, Request
 from util import even_split
 from peer import Peer
 
-class Lkjc(Peer):
+class LkjcStd(Peer):
     def post_init(self):
         print "post_init(): %s here!" % self.id
         self.dummy_state = dict()
@@ -32,41 +32,55 @@ class Lkjc(Peer):
         needed_pieces = filter(needed, range(len(self.pieces)))
         np_set = set(needed_pieces)  # sets support fast intersection ops.
 
-
-        logging.debug("%s here: still need pieces %s" % (
-            self.id, needed_pieces))
-
-        logging.debug("%s still here. Here are some peers:" % self.id)
-        for p in peers:
-            logging.debug("id: %s, available pieces: %s" % (p.id, p.available_pieces))
-
-        logging.debug("And look, I have my entire history available too:")
-        logging.debug("look at the AgentHistory class in history.py for details")
-        logging.debug(str(history))
-
         requests = []   # We'll put all the things we want here
         # Symmetry breaking is good...
         random.shuffle(needed_pieces)
-        
-        # Sort peers by id.  This is probably not a useful sort, but other 
-        # sorts might be useful
-        peers.sort(key=lambda p: p.id)
+
         # request all available pieces from all peers!
         # (up to self.max_requests from each)
+
+        # rarest first strategy
+        piece_peers = dict()
+        piece_counts = dict()
+        n_requests = dict()
+
+        for peer in peers:
+            av_set = set(peer.available_pieces)
+            isect = av_set.intersection(np_set)
+            n_requests[peer.id] = 0
+            # n = min(self.max_requests, len(isect))
+            for piece_id in list(isect):
+                if piece_id not in piece_peers:
+                    piece_peers[piece_id] = [peer.id]
+                    piece_counts[piece_id] = 0
+                else:
+                    piece_peers[piece_id].append(peer.id)
+                    piece_counts[piece_id] += 0
+
+        counts_piece = dict()
+        for piece, count in piece_counts.items():
+            if count in counts_piece:
+                counts_piece[count].append(piece)
+            else:
+                counts_piece[count] = [piece]
+
+        rarest = sorted(counts_piece.items())
+
+
         for peer in peers:
             av_set = set(peer.available_pieces)
             isect = av_set.intersection(np_set)
             n = min(self.max_requests, len(isect))
-            # More symmetry breaking -- ask for random pieces.
-            # This would be the place to try fancier piece-requesting strategies
-            # to avoid getting the same thing from multiple peers at a time.
-            for piece_id in random.sample(isect, n):
-                # aha! The peer has this piece! Request it.
-                # which part of the piece do we need next?
-                # (must get the next-needed blocks in order)
-                start_block = self.pieces[piece_id]
-                r = Request(self.id, peer.id, piece_id, start_block)
-                requests.append(r)
+            count = 0
+            while count < n:
+                for c, piece_id_list in rarest:
+                    random.shuffle(piece_id_list)
+                    for piece_id in piece_id_list:
+                        if piece_id in av_set:
+                            count += 1
+                            start_block = self.pieces[piece_id]
+                            r = Request(self.id, peer.id, piece_id, start_block)
+                            requests.append(r)
 
         return requests
 
