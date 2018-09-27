f91b090b3cb07760fa3b9644fc0dc7052ca5f65b
janetchen1 <janetchen@college.harvard.edu>
2018-09-26 23:06:34 -0400

    tourney

--- lkjctourney.py
+++ lkjctourney.py
@@ -8,16 +8,24 @@
 
 import random
 import logging
+from datetime import datetime
 
 from messages import Upload, Request
 from util import even_split
 from peer import Peer
 
-class Lkjctourney(Peer):
+class LkjcTourney(Peer):
     def post_init(self):
         print "post_init(): %s here!" % self.id
         self.dummy_state = dict()
         self.dummy_state["cake"] = "lie"
+        self.gamma = 0.1
+        self.r = 3
+        self.alpha = 0.2
+        self.cap = self.up_bw
+        self.flows = dict()
+        self.taus = dict()
+        self.unchoked_past = dict()
     
     def requests(self, peers, history):
         """
@@ -28,6 +36,7 @@ class Lkjctourney(Peer):
 
         This will be called after update_pieces() with the most recent state.
         """
+
         needed = lambda i: self.pieces[i] < self.conf.blocks_per_piece
         needed_pieces = filter(needed, range(len(self.pieces)))
         np_set = set(needed_pieces)  # sets support fast intersection ops.
@@ -53,21 +62,31 @@ class Lkjctourney(Peer):
         peers.sort(key=lambda p: p.id)
         # request all available pieces from all peers!
         # (up to self.max_requests from each)
+
+        # rarest first strategy
+        piece_peers = dict()
+        n_requests = dict()
+
         for peer in peers:
             av_set = set(peer.available_pieces)
             isect = av_set.intersection(np_set)
-            n = min(self.max_requests, len(isect))
-            # More symmetry breaking -- ask for random pieces.
-            # This would be the place to try fancier piece-requesting strategies
-            # to avoid getting the same thing from multiple peers at a time.
-            for piece_id in random.sample(isect, n):
-                # aha! The peer has this piece! Request it.
-                # which part of the piece do we need next?
-                # (must get the next-needed blocks in order)
-                start_block = self.pieces[piece_id]
-                r = Request(self.id, peer.id, piece_id, start_block)
-                requests.append(r)
+            n_requests[peer.id] = 0
+            # n = min(self.max_requests, len(isect))
+            for piece_id in list(isect):
+                if piece_id not in piece_peers:
+                    piece_peers[piece_id] = [peer]
+                else:
+                    piece_peers[piece_id].append(peer)
 
+        # sort pieces by rarest
+        rarest = sorted(piece_peers, key=lambda k: len(piece_peers[k]), reverse=False)
+        for piece in rarest:
+            for peer in piece_peers[piece]:
+                if n_requests[peer.id] < self.max_requests:
+                    n_requests[peer.id] += 1
+                    start_block = self.pieces[piece_id]
+                    r = Request(self.id, peer.id, piece_id, start_block)
+                    requests.append(r)
         return requests
 
     def uploads(self, requests, peers, history):
@@ -89,22 +108,96 @@ class Lkjctourney(Peer):
         # has a list of Download objects for each Download to this peer in
         # the previous round.
 
+         # Initialize f_j and t_j for all peers
+        if round == 0:
+            for peer in peers:
+                self.flows[peer.id] = self.up_bw / 4
+                self.taus[peer.id] = 1
+                # how many times has this peer unchoked me before?
+                self.unchoked_past[peer.id] = 0
+
+        chosen = []
+        bws = []
         if len(requests) == 0:
             logging.debug("No one wants my pieces!")
-            chosen = []
-            bws = []
         else:
+            request_ids = []
+            for request in requests:
+                request_ids.append(request.requester_id)
+
+            # step 4
+            # sort peers by decreasing ratio of reciprocation likelihood
+            ratios = dict()
+            for peer in request_ids:
+                ratios[peer] = self.flows[peer]/self.taus[peer]
+
+            # assign "importance" indexes to peers based on how many pieces they have that I need
+            needed = lambda i: self.pieces[i] < self.conf.blocks_per_piece
+            needed_pieces = filter(needed, range(len(self.pieces)))
+            np_set = set(needed_pieces)  # sets support fast intersection ops.
+
+            peer_need_overlap = dict()
+
+            for peer in peers:
+                if peer in request_ids:
+                    av_set = set(peer.available_pieces)
+                    isect = av_set.intersection(np_set)
+                    peer_need_overlap[peer] = len(list(isect))
+
+            importance = sorted(peer_need_overlap, key=lambda k: peer_need_overlap[k])
+
+            # pick uploads
+            ul = 0
+            sorted_peers = sorted(ratios, key=lambda k: ratios[k], reverse=False)
+            logging.debug(sorted_peers)
+            random.seed(datetime.now())
+            while ul < self.cap and len(sorted_peers) > 0:
+                # randomly vary to sometimes choose "important" peers over most efficient
+                if random.random() <= 0.02 * len(chosen) and len(importance) > 0:
+                    candidate = importance.pop()
+                    while candidate in chosen:
+                        candidate = importance.pop()
+                    # eliminate chance for redundancy
+                else:
+                    candidate = sorted_peers.pop()
+                    while candidate in chosen:
+                        candidate = sorted_peers.pop()
+                if (ul + self.taus[candidate]) < self.cap:
+                    chosen.append(candidate)
+                    bws.append(self.taus[candidate])
+                ul += self.taus[candidate]
+
+            """
             logging.debug("Still here: uploading to a random peer")
             # change my internal state for no reason
             self.dummy_state["cake"] = "pie"
-
-            request = random.choice(requests)
-            chosen = [request.requester_id]
-            # Evenly "split" my upload bandwidth among the one chosen requester
-            bws = even_split(self.up_bw, len(chosen))
-
+            """
+            
         # create actual uploads out of the list of peer ids and bandwidths
         uploads = [Upload(self.id, peer_id, bw)
                    for (peer_id, bw) in zip(chosen, bws)]
+
+        if round == 0:
+            return uploads
+
+        # Step 5
+
+        # find peers who unchoked me and update
+        unchokers = set()
+        for dl in history.downloads[round-1]:
+            # update flow with observed flow
+            self.flows[dl.from_id] = dl.blocks
+            unchokers.add(dl.from_id)
+            # increment unchoked_past
+            self.unchoked_past[dl.from_id] += 1
+            # if peer j has unchoked i for each of last r rounds, then decrease tau_j
+            if self.unchoked_past[dl.from_id] > self.r:
+                self.taus[dl.from_id] *= (1-self.gamma)
+
+        # update tau and unchoked_past peers who didn't unchoke me
+        others = list(set(chosen)-unchokers)
+        for j in others:
+            self.taus[j] *= (1+self.alpha)
+            self.unchoked_past[j] = 0
             
         return uploads
